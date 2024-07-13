8b8a9a5d93590487a696f3e095c51bb58e61da52
Floris <floris@xylex.ai>
2024-07-14 00:55:08 +0200

    auth

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 watchdog_rs
 
 port 4035
+
+
+
+GNU nano 6.2 /etc/nginx/sites-available/WILDCARD.DOMAIN.TLD 
+
+server { 
+    listen 80; 
+    server_name WILDCARD.DOMAIN.TLD; 
+    location / { 
+        proxy_pass http://127.0.0.1:PORT; 
+        proxy_set_header Host $host; 
+        proxy_set_header X-Real-IP $remote_addr; 
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; 
+        proxy_set_header X-Forwarded-Proto $scheme; 
+    } 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,41 @@
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use serde_json::json;
+use std::collections::HashMap;
+use std::env::var;
+use std::io::Result;
+
+#[get("/ping")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().json(json!({"message": "Hello world!"}))
+}
+
+#[post("/build")]
+async fn build(query: web::Query<HashMap<String, String>>) -> impl Responder {
+    dotenv().ok();
+    let required_build_key: String = var("BUILD_KEY").expect("BUILD_KEY must be set");
+
+    let default_build_id: String = "0".to_string();
+    let build_id: &String = query.get("build_id").unwrap_or(&default_build_id);
+
+    if let Some(build_key) = query.get("build_key") {
+        if build_key == &required_build_key {
+            return HttpResponse::Ok().json(json!(
+                {"message": "buidl!", "build_id": build_id}
+            ));
+        }
+    }
+    HttpResponse::Unauthorized().json(json!(
+        {"error": "Unauthorized"}
+    ))
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    println!("\x1b[1;32mServer running\x1b[0m");
+
+    HttpServer::new(|| App::new().service(index).service(build))
+        .bind(("127.0.0.1", 4035))?
+        .run()
+        .await
 }
